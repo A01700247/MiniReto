dd582e503315e80f30d5ad20dce402deab85fcaf
Diegomg <A01700247@itesm.mx>
2020-04-09 23:19:57 -0500

    Initial Commit

---
+++ Mini reto/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Mini reto
+//
+//  Created by Diego Macias on 09/04/20.
+//
+
+import Foundation
+import Cocoa
+
+for num in 0...100{
+    if num % 5 == 0{
+        print("\(num)" + " " + "Bingo!!!")
+    } else if num % 2 == 0 {
+        print("\(num)" + " " + "Par")
+    } else if num % 2 != 0 {
+        print("\(num)" + " " + "Impar")
+    } else{
+        print("Numero fuera del rango")
+    }
+    if num >= 30 && num <= 40 {
+        print("\(num)" + " " + "Viva Swift")
+    }
+}
+
